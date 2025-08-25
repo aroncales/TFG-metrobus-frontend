a5328c044bf8d1808c3371d10b5c226316295aa7
aroncales <aroncalesguillen@gmail.com>
2025-08-26 01:05:28 +0200

    First Commit: Front-end React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import TopBar from './topbar/components/TopBar';
+
+function App() {
+
+  return (
+    <>
+    {/* Barra Superior Logo + Busqueda */}
+    <TopBar></TopBar>
+    {/* CONTENT */}
+    {/* Barra inferior opciones */}
+    </>
+  )
+}
+
+export default App;
